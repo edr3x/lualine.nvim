2e53d2002b40f01f9854b3d688ff1b465ac9f2a1
edr3x <dhunganaannanta@gmail.com>
2023-01-22 22:56:00 +0545

    feat: custom-tokyo

---
+++ lua/lualine/themes/custom_tokyonight.lua
@@ -0,0 +1,32 @@
+local colors = {
+    highlight = "#89DDFF",
+    bg        = "#1A1B26",
+    fg        = "#B0BEC5",
+    fg2       = '#3e445e',
+    orange    = "#e78a4e",
+}
+
+return {
+    normal = {
+        a = { fg = colors.highlight, bg = colors.bg, gui = 'bold' },
+        b = { fg = colors.orange, bg = colors.bg },
+        c = { fg = colors.color2, bg = colors.bg },
+    },
+    insert = {
+        a = { fg = colors.highlight, bg = colors.bg, gui = 'bold' },
+        b = { fg = colors.orange, bg = colors.bg },
+    },
+    visual = {
+        a = { fg = colors.highlight, bg = colors.bg, gui = 'bold' },
+        b = { fg = colors.orange, bg = colors.bg },
+    },
+    replace = {
+        a = { fg = colors.highlight, bg = colors.bg, gui = 'bold' },
+        b = { fg = colors.orange, bg = colors.bg },
+    },
+    inactive = {
+        c = { fg = colors.fg2, bg = colors.bg },
+        a = { fg = colors.fg2, bg = colors.bg, gui = 'bold' },
+        b = { fg = colors.fg2, bg = colors.bg },
+    },
+}
